03f57951d0b253737ed3699ac8f4ce0300bd8c1a
claudio <claudio>
2010-04-15 15:37:51 +0000

    session_shutdown() issues a NBR_EVT_CLOSE_SESSION and therefore a session_close() which does the event_del(&nbr->rev) so no need to do it twice. There is also no need to send two S_SHUTDOWN messages. OK michele@

--- packet.c
+++ packet.c
@@ -325,8 +325,6 @@ session_read(int fd, short event, void *arg)
 	if ((n = read(fd, nbr->rbuf->buf + nbr->rbuf->wpos,
 	    sizeof(nbr->rbuf->buf) - nbr->rbuf->wpos)) == -1) {
 		if (errno != EINTR && errno != EAGAIN) {
-			/* XXX find better error */
-			event_del(&nbr->rev);
 			session_shutdown(nbr, S_SHUTDOWN, 0, 0);
 			return;
 		}
@@ -335,7 +333,6 @@ session_read(int fd, short event, void *arg)
 	}
 	if (n == 0) {
 		/* connection closed */
-		event_del(&nbr->rev);
 		session_shutdown(nbr, S_SHUTDOWN, 0, 0);
 		return;
 	}
@@ -450,8 +447,12 @@ void
 session_shutdown(struct nbr *nbr, u_int32_t status, u_int32_t msgid,
     u_int32_t type)
 {
+	log_debug("session_shutdown: nbr ID %s, status %x",
+	    inet_ntoa(nbr->id), status);
+
 	send_notification_nbr(nbr, status, msgid, type);
-	send_notification_nbr(nbr, S_SHUTDOWN, msgid, type);
+	if (status != S_SHUTDOWN)
+		send_notification_nbr(nbr, S_SHUTDOWN, msgid, type);
 
 	nbr_fsm(nbr, NBR_EVT_CLOSE_SESSION);
 }
