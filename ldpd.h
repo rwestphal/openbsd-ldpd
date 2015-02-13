c69db01d733c0bcbc66da756f13818f75fd83318
Renato Westphal <renato@openbsd.org>
2015-02-12 22:00:00 -0200

    Rework kroute.c to send only the best routes to lde.
    This is major rework of the kroute.c code. The idea is remove complexity
    from the lde process by making kroute.c advertise only the lowest priority
    route of each prefix.
    
    kroute.c keeps track of all routes using three different structures:
    kroute_prefix, kroute_priority and kroute_node.
    
    kroute_prefix represents a prefix and contains an ordered list of
    priorities (kroute_priority) and for each priority there is a list of
    nexthops (kroute_node). Arranging the routes using these three structures
    allows for the writing of a simpler code, easier to understand.
    
    Whenever a route is removed, if there's another route for the same prefix,
    but with a lower priority, this route is immediately sent to lde.
    
    Additional fixes:
    * On RTM_CHANGE, remove the old route before installing the new one;
    * On IMSG_CTL_KROUTE_ADDR, show all nexthops for multpath routes;

--- ldpd.h
+++ ldpd.h
@@ -347,9 +347,8 @@ void		 kr_show_route(struct imsg *);
 void		 kr_ifinfo(char *, pid_t);
 struct kif	*kif_findname(char *);
 void		 kr_reload(void);
-
-u_int8_t	mask2prefixlen(in_addr_t);
-in_addr_t	prefixlen2mask(u_int8_t);
+u_int8_t	 mask2prefixlen(in_addr_t);
+in_addr_t	 prefixlen2mask(u_int8_t);
 
 /* log.h */
 const char	*nbr_state_name(int);
