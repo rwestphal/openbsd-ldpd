a162fd3b54c24fa2562cef1a1b5cdaf69db6fb3f
renato <renato>
2015-03-21 18:34:01 +0000

    Remove unused variables and functions.

--- lde.c
+++ lde.c
@@ -198,10 +198,8 @@ lde_dispatch_imsg(int fd, short event, void *bula)
 	struct imsg		 imsg;
 	struct lde_nbr		 *nbr;
 	struct map		 map;
-	struct timespec		 tp;
 	struct in_addr		 addr;
 	ssize_t			 n;
-	time_t			 now;
 	int			 shut = 0, verbose;
 
 	if (event & EV_READ) {
@@ -216,9 +214,6 @@ lde_dispatch_imsg(int fd, short event, void *bula)
 		if (n == 0)	/* connection closed */
 			shut = 1;
 	}
-
-	clock_gettime(CLOCK_MONOTONIC, &tp);
-	now = tp.tv_sec;
 
 	for (;;) {
 		if ((n = imsg_get(ibuf, &imsg)) == -1)
