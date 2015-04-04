322ab80c28ae7af9a88c60f9b2767512c31d33d0
Renato Westphal <renato@openbsd.org>
2015-04-04 15:15:44 +0000

    Show the full LIB in the "ldpctl show lib" command.
    The LIB is a table where the router keeps all known MPLS labels. So,
    we should loop over all the received label mappings from all neighbors
    to show the full LIB.
    
    The lde_nbr_is_nexthop() function was introduced to verify if a lib
    entry is supposed to be installed in the fib (according to the fib entry's
    nexthop and the addresses advertised by the lib entry's nexthop). This is
    better than keeping track of lib<->fib entries with pointers and back
    pointers because it keeps the lib/fib structures independent of each
    other, which in turn makes the code less prone to bugs.
    
    OK claudio@

--- ldpd.h
+++ ldpd.h
@@ -321,14 +321,11 @@ struct ctl_nbr {
 
 struct ctl_rt {
 	struct in_addr		 prefix;
+	u_int8_t		 prefixlen;
 	struct in_addr		 nexthop;
-	struct in_addr		 adv_rtr;
-	time_t			 uptime;
 	u_int32_t		 local_label;
 	u_int32_t		 remote_label;
 	u_int8_t		 flags;
-	u_int8_t		 prefixlen;
-	u_int8_t		 connected;
 	u_int8_t		 in_use;
 };
 
