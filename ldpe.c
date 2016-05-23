ac45fc3b5e5ed32d728a01fba2e2d5a1f1ea7413
Renato Westphal <renato@openbsd.org>
2016-05-23 15:51:36 +0000

    pledge() earlier on ldpe.

--- ldpe.c
+++ ldpe.c
@@ -204,6 +204,9 @@ ldpe(struct ldpd_conf *xconf, int pipe_parent2ldpe[2], int pipe_ldpe2lde[2],
 	    setresuid(pw->pw_uid, pw->pw_uid, pw->pw_uid))
 		fatal("can't drop privileges");
 
+	if (pledge("stdio cpath inet mcast", NULL) == -1)
+		fatal("pledge");
+
 	event_init();
 	accept_init();
 
@@ -267,9 +270,6 @@ ldpe(struct ldpd_conf *xconf, int pipe_parent2ldpe[2], int pipe_ldpe2lde[2],
 	/* start configured targeted neighbors */
 	LIST_FOREACH(tnbr, &leconf->tnbr_list, entry)
 		tnbr_init(xconf, tnbr);
-
-	if (pledge("stdio cpath inet mcast", NULL) == -1)
-		fatal("pledge");
 
 	event_dispatch();
 
