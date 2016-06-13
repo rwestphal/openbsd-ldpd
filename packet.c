88e7b18d7e31c713f97056d3ea6f61892b4f74bf
Renato Westphal <renato@openbsd.org>
2016-06-13 20:13:34 +0000

    Rework the handling of Hello packets.
    With the introduction of IPv6 support by RFC 7552, the handling of Hello
    packets in ldpd became something incredibly complex. Neighbors can change
    from single-stack LDP to dual-stack and vice-versa. They can change
    their transport preference, their transport addresses (IPv4 and IPv6)
    and even start or stop sending the Dual-Stack TLV. We also have to take
    care to reject things like multiple adjacencies advertising different
    transport-addresses for the same neighbor. ldpd was failing for some of
    the cases mentioned above, this patch fixes these issues and attempts
    to make the code easier to read.

--- packet.c
+++ packet.c
@@ -624,7 +624,7 @@ session_shutdown(struct nbr *nbr, uint32_t status, uint32_t msgid,
 	case NBR_STA_INITIAL:
 	case NBR_STA_OPENREC:
 	case NBR_STA_OPENSENT:
-	case NBR_STA_OPER:	
+	case NBR_STA_OPER:
 		log_debug("%s: lsr-id %s", __func__, inet_ntoa(nbr->id));
 
 		send_notification_nbr(nbr, status, msgid, type);
